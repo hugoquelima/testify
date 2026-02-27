c0aedc6d536283bb4238a2f537cc43a791cfff18
Hugo <hugo2006queiroz@gmail.com>
2026-02-27 18:42:38 +0000

    Fix Next.js config for Vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
